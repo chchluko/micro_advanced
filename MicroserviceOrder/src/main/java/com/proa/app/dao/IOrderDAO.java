f8f4cdbad751931522d68e774cd1379bebe8b3b3
jesus <chucheriasdehoy@gmail.com>
2025-03-14 15:55:12 -0600

    se inicia el feint 2 metodos

---
+++ MicroserviceOrder/src/main/java/com/proa/app/dao/IOrderDAO.java
@@ -0,0 +1,10 @@
+package com.proa.app.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.proa.app.entities.Order;
+
+public interface IOrderDAO extends CrudRepository <Order, Long> {
+		
+
+}
